6242608c5a086bc9dd8fe263e85e0daffcd17846
emarino-git <82189750+emarino-git@users.noreply.github.com>
2022-07-03 17:21:53 +0000

    first commit

---
+++ src/components/TaskForm.js
@@ -0,0 +1,38 @@
+import { useState } from "react"
+import { useDispatch } from "react-redux"
+import { addTask } from "../features/tasks/taskSlice"
+import { v4 as uuid } from 'uuid'
+
+function TaskForm() {
+  const [task, setTask] = useState({
+    title: '',
+    description: ''
+  })
+
+  const dispatch = useDispatch()
+
+  const handleChange = e => {
+    setTask({
+      ...task,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    dispatch(addTask({
+      ...task,
+      id: uuid(),
+    }))
+  }
+
+  return (
+    <form onSubmit={handleSubmit} >
+      <input name='title' type="text" placeholder="title" onChange={handleChange} />
+      <textarea name='description' placeholder='description' onChange={handleChange} />
+      <button>Save</button>
+    </form>
+  )
+}
+
+export default TaskForm
---
+++ src/components/TaskList.js
@@ -0,0 +1,19 @@
+import { useSelector } from "react-redux"
+
+
+export default function TaskList() {
+
+  const tasks = useSelector(state => state.tasks)
+  console.log(tasks)
+
+  return(
+    <div>
+    {tasks.map(task => (
+      <div key={task.id}>
+        <h3>{task.title}</h3>
+        <p>{task.description}</p>
+      </div>
+    ))}
+    </div>
+  )
+}
